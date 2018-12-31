be08dc4013ab5da1666686a6bc44e252838cb71b
Gerlie Traya <navasquezgerlie@yahoo.com>
2018-12-31 13:25:57 +0100

    added some object methods

---
+++ methods.js
@@ -0,0 +1,10 @@
+let playTime = {
+    walk : function(){
+        console.log('Lula, lets go for a walk!')
+    },
+    jog : function(){
+        console.log('Lula, lets go for a run!')
+    }
+};
+playTime.walk();
+playTime.jog();
