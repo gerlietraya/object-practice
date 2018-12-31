e67de70aae00322aa8993fd52b59bff29f3a3c69
Gerlie Traya <navasquezgerlie@yahoo.com>
2018-12-31 12:56:41 +0100

    added main.js with objects and an index.html file to load on the browser

---
+++ main.js
@@ -0,0 +1,59 @@
+let lachmannHousehold = {
+    'family members' : {
+        Dad : {
+            name : 'Jacob',
+            job : 'financial adviser',
+            age : 49,
+            hobby: 'work-out'
+        },
+        Mom : {
+            name : 'Louise',
+            job : 'CEO',
+            age : 45,
+            hobby: 'work-out'
+        },
+         
+        Children : {
+            daughter : {
+                name : 'Lucca',
+                job : 'student',
+                age : 13,
+                hobby : 'tennis'
+            },
+            son : {
+                name:'Louis',
+                job :'student',
+                age : 11,
+                hobby:'football',
+
+            },
+
+        },
+        Aupair: {
+            name : 'Gerlie',
+            job : 'chambermaid',
+            age : 28,
+            hobby : 'coding'
+        },
+        pets : {
+            giant : {
+                name : 'lula',
+                job : 'dog',
+                age : 4,
+                hobby : 'sleep',  
+            },
+            
+
+
+            tiny : {
+                name : 'Fnuller',
+                job : 'rabbit',
+                age : 1,
+                hobby : 'chew'
+            }
+            
+        }
+
+    }
+};
+console.log(lachmannHousehold);
